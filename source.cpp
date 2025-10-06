f98a63e84720eaa3a4de4a4dcb6c0e804f7102f1
FaithVVV <ajdvilain@gmail.com>
2025-10-06 17:42:59 -0400

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -1,6 +1,7 @@
-
 using namespace std;
+#include <cmath>
 #include <iostream>
+#include <cstdlib>
 
 void printBoxGraph(int displayNumber)
 {
@@ -16,12 +17,6 @@ void printBoxGraph(int displayNumber)
 
 int main()
 {
-  int x = 6; //x & y axis
-  int y = 6;
-  
-  int center[2] = {(y/2),(x/2)}; //center coordinate's
-
-  int graphPlot[x][y] = {0}; //initialise all locations in array to 0
 
   int xChoose = 0; //player assigned plot locations
   int yChoose = 0;
@@ -31,6 +26,24 @@ int main()
   
   cout<<"y; ";
   cin>>yChoose;
+  
+  int x = 5;
+  int y = 5;
+
+  if (xChoose>(x-1)/2 || xChoose<-1*((x-1)/2))
+  {
+    x = abs(xChoose) * 2 + 1;
+  }
+  if (xChoose>(y-1)/2 || yChoose<-1*((y-1)/2))
+  {
+    y = abs(xChoose) * 2 + 1;
+  }
+
+  int center[2] = {(y/2),(x/2)}; //center coordinate's
+
+  int graphPlot[x][y] = {0}; //initialise all locations in array to 0
+
+
 
   graphPlot[center[0] - yChoose][center[1] + xChoose] = 1; //equation to take player input and plot
    
